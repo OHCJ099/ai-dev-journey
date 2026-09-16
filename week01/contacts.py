060846e3c2ab0e09227a27f7c01455ef376fadb7
OHCJ099 <564494689@qq.com>
2026-09-16 14:19:43 +0800

    feat: json load

--- week01/contacts.py
+++ week01/contacts.py
@@ -1,4 +1,11 @@
-contacts = []
+import json
+from pathlib import Path
+
+DATA_FILE = Path(__file__).parent / "contacts.json"
+if DATA_FILE.exists():
+    contacts = json.loads(DATA_FILE.read_text(encoding="utf-8"))
+else:
+    contacts = []
 
 while True:
     line = input("> ")
