5d3be8638363a184ff7da54e8e4a1d9f04a4fb33
OHCJ099 <564494689@qq.com>
2026-09-17 13:16:06 +0800

    feat: 添加完善delete分支

--- week01/contacts.py
+++ week01/contacts.py
@@ -11,10 +11,6 @@ else:
 # 主循环
 while True:
     line = input("> ")
-    # quit分支
-    if line == "quit":
-        DATA_FILE.write_text(json.dumps(contacts, ensure_ascii=False, indent=2), encoding="utf-8")
-        break
 
     # add分支
     if line.startswith("add "):
@@ -24,6 +20,22 @@ while True:
             continue
         contacts.append({"name": parts[1], "phone": parts[2]})   # ← append 归位到 add 分支内
 
+    # delete分支
+    elif line.startswith("del "):
+        parts = line.split()
+        if len(parts) != 2:
+            print("格式：del 序号")
+            continue
+
+        try:
+            del_index = int(parts[1])
+        except ValueError:
+            print("请输入数字")
+        if len(contacts) >= del_index >= 1:
+            del_user = contacts.pop(del_index - 1)
+            print(f"已删除 {del_user['name']} {del_user['phone']}")
+        else:
+            print("数字不合法")        
     # list分支
     elif line == "list":
         for index, contact in enumerate(contacts, start = 1):
@@ -44,6 +56,11 @@ while True:
                 found = True
         if not found:
             print(f"没找到{name}")
-    
+
+    # quit分支
+    elif line == "quit":
+        DATA_FILE.write_text(json.dumps(contacts, ensure_ascii=False, indent=2), encoding="utf-8")
+        break
+
     else:
         print("unknown command")
