7bc3150ee57d804b04631922b5b2df4cec78c696
OHCJ099 <564494689@qq.com>
2026-09-18 15:33:16 +0800

    fix: delete_contact 改收用户号，边界条件修正 off-by-one

--- week01/contacts/core.py
+++ week01/contacts/core.py
@@ -6,3 +6,15 @@ def list_contacts(contacts: list[dict[str, str]]) -> list[tuple[int, dict[str, s
     for index, contact in enumerate(contacts ,start=1):
         new_list.append((index, contact))
     return new_list
+
+def find_contacts(contacts: list[dict[str, str]], keyword: str) -> list[tuple[int, dict[str, str]]]:
+    new_list = []
+    for index, contact in enumerate(contacts, start=1):
+        if keyword in contact["name"]:
+            new_list.append((index, contact))
+    return new_list
+
+def delete_contact(contacts: list[dict[str, str]], user_no: int) -> dict[str, str] | None:
+    if 1 <= user_no <= len(contacts):
+        return contacts.pop(user_no - 1)
+    return None
