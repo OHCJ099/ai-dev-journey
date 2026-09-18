7dd06e53e14b5032f49e57b5bc9fa835caec7e4c
OHCJ099 <564494689@qq.com>
2026-09-18 14:28:48 +0800

    fix: 删掉 core.py 里 import 时就会执行的测试 print

--- week01/contacts/core.py
+++ week01/contacts/core.py
@@ -3,8 +3,6 @@ def add_contact(contacts: list[dict[str, str]], name: str, phone: str) -> None:
 
 def list_contacts(contacts: list[dict[str, str]]) -> list[tuple[int, dict[str, str]]]:
     new_list = []
-    for index, contact in enumerate(contacts , start=1):
+    for index, contact in enumerate(contacts ,start=1):
         new_list.append((index, contact))
     return new_list
-
-print(list_contacts([{"name":"李四", "phone": "139"},{"name":"王五", "phone": "193"}]))
